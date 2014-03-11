f7a5cf0c989cf73cd19d4dde429c9a016738b666
Alexandre Moraes <kalvinmoraes@gmail.com>
2014-03-11 20:22:30 -0300

    Added files folder as default

--- app/Config/core.php
+++ app/Config/core.php
@@ -226,12 +226,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', '6PkmwNcwNvt3SzRie5y6QDjNaijeaHjKkVvbCyCU');
+	Configure::write('Security.salt', 'jMy05ItOloewb0E6ai61qtqgwfWhyRw281u0lWIb');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '35757788220474066504907922424');
+	Configure::write('Security.cipherSeed', '61383252085869149475951119677');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
